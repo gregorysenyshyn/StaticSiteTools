a80fe3320e402880391bd74716a9812ffd644db7
Gregory Senyshyn <greg@gregorysenyshyn.com>
2020-03-26 12:55:29 -0400

    cleaning up whitespace, etc.

--- build.py
+++ build.py
@@ -5,18 +5,19 @@ import argparse
 
 import tools
 
+
 def build(data):
 
     print('\nStarting build!')
     t0 = time.time()
-    
+
     data = tools.load_yaml(data)
-    
+
     print('\n\n=== C L E A N ===')
     print(f'cleaning {data["options"]["dist"]}...', end='')
     tools.clean(data['options']['dist'])
     print(' Done!')
-    
+
     print('\n\n=== J S ===')
     for dest_path in data['js']['paths']:
         tools.handle_js(data, dest_path)
@@ -32,15 +33,16 @@ def build(data):
     print(f'Built all pages in {round(float(time.time() - t1), 4)} seconds')
 
     print('\n\n=== I M A G E S ===')
-    tools.handle_images(data['options']) 
+    tools.handle_images(data['options'])
 
     print('\n\n=== Entire build done in',
           f'{round(float(time.time() - t0), 4)} seconds ===')
 
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', help = 'YAML data file')
+    parser.add_argument('--data', help='YAML data file')
     args = parser.parse_args()
-    
+
     build(args.data)
